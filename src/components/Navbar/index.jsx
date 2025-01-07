60ce05804e5f31f95b237341d18079bf9e602648
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-01-07 16:42:14 +0700

    Fixing Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import MaxWidthWrapper from '../MaxWidthWrapper'
+import IconKnc from '../../assets/IconKnc.png'
+import { ShoppingCart } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
     return(
-     <div className="flex items-center h-12 ">
+     <div className="flex bg-black items-center h-14">
         <MaxWidthWrapper className="flex justify-between items-center">
-            <h2>a</h2>
-            <div className="navigation">
-                
+            <img onClick={() => window.location.href = '/'} className='w-8 cursor-pointer' src={IconKnc} alt="KncStudio"/>
+            <div className="flex items-center  text-white">
+              <div className="border-r">
+               <a className='m-9 hover:text-purple-300' href="">Sign In</a>
+              </div>
+              <div className="border-r">
+               <a className='m-9  hover:text-purple-300' href="">Create Account</a>
+              </div>
+              <ShoppingCart className='ml-6 cursor-pointer  hover:text-purple-300'/> <span className='ml-3 flex items-center'>0</span>
             </div>
         </MaxWidthWrapper>
      </div>
