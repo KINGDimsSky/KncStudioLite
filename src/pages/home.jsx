17a6ec9e33d97fdb995f6885548637ee3af992f8
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-01-07 15:10:48 +0700

    First Commit cak

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
-        <h2 className='text-white text-6xl'>SYAHRUL ANJING</h2>
+        <h2 className='text-white text-6xl'>SYAHRUL ANJING!</h2>
     </div>
   )
 }
