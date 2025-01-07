e36bc87d3faad6747c4e15cd9727f6778337cd13
KINGDimsSky <dimasmarcelo2004@gmail.com>
2025-01-07 17:39:08 +0700

    Adding Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import IconKnc from '../assets/IconKnc.png'
+import { ArrowRight } from 'lucide-react'
+
 
 const Footer = () => {
   return (
-    <div className="flex flex-col h-12 bg-black">
-        <MaxWidthWrapper className="flex items-center justify-center border-t border-gray-700">
-            <h2 className='text-white'>Knc Studio</h2>
+    <div className="flex flex-col h-[29rem] bg-black">
+        <MaxWidthWrapper className="flex flex-col items-center justify-center border-t border-gray-700">
+            <div className="mt-24">
+              <img className='w-12' src={IconKnc} alt="" />
+            </div>
+            <div className="titleFooter">
+              <h2 className='text-white mt-11'>Become a Seller</h2>
+            </div>
+            <div className="flex flex-col items-center">
+              <p className='text-gray-300 mt-4 tracking-tight font-extralight w-[29rem] text-center'>If you'd like to sell high-quality digital products, you can do so</p>
+              <div className="flex gap-1 ">
+                <p className='text-gray-300 tracking-tight font-extralight'>in minutes. <span className='font-bold text-white'>Get Started</span></p>
+                <ArrowRight className='text-white w-4'/>
+              </div>
+            </div>
+            <div className="flex justify-between w-full mt-36">
+              <p className='text-gray-300 font-extralight tracking-tight'>© 2025 All Rights Reserved</p> 
+              <div className="flex text-sm gap-8 text-gray-300 font-extralight tracking-tight">
+                <a href="">Terms</a>
+                <a href="">Privacy Policy</a>
+                <a href="">Cookie Policy</a>
+              </div>
+            </div>
         </MaxWidthWrapper>
     </div>
   )
